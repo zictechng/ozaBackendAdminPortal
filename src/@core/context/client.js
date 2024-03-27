e4e5cbaaf53925fed15559fe0f33b43d37f4b25a
Zictech Technologies <zictechng@gmail.com>
2024-03-27 21:18:34 +0000

    change backed

--- src/@core/context/client.js
+++ src/@core/context/client.js
@@ -9,8 +9,8 @@ import axios from "axios";
 
 //export default axios.create({baseURL: 'https://crane-cuff-links.cyclic.cloud'})
 
-export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
+//export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
 
-//export default axios.create({baseURL: 'https://ozawebservice.onrender.com'})
+export default axios.create({baseURL: 'https://ozawebservice.onrender.com'})
 
 
